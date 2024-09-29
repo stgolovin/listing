a460288d84a357e50ef766ced6b8390bf6347189
Stanislav Golovin <golovin.ds93@gmail.com>
2024-09-29 14:59:51 +0300

    add exception for eslint

--- src/components/ItemQuantity.tsx
+++ src/components/ItemQuantity.tsx
@@ -4,8 +4,11 @@ export type ItemQuantityType = {
   
 export const ItemQuantity = ({quantity}: ItemQuantityType) => {
     const className = (
+      // eslint-disable-next-line no-constant-binary-expression
       (quantity === undefined) ? 'level-high' : null ||
+      // eslint-disable-next-line no-constant-binary-expression
       (quantity <= 10) ? 'level-low' : null ||
+      // eslint-disable-next-line no-constant-binary-expression
       (quantity <= 20) ? 'level-medium' : null ||
       'level-high'
     );
@@ -13,4 +16,4 @@ export const ItemQuantity = ({quantity}: ItemQuantityType) => {
     return (
       <p className={"item-quantity " + className}>{quantity}</p>
     )
-}
+  }
